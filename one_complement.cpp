094ed9be657877b710e882a86c584cd37b4fee96
aakashyap <akash.1234@live.com>
2020-05-07 23:37:48 +0530

    Day 6

--- one_complement.cpp
+++ one_complement.cpp
@@ -27,7 +27,6 @@ class Solution {
 public:
     int findComplement(int num) {
         int nbits = log2(num) + 1;
-        cout<<nbits<<endl;
         int allones = static_cast<unsigned>(1 << nbits) - 1;
         return allones^num;
     }
