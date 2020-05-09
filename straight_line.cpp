dc089b81022db0ffd3f67682d313aa45a912d85c
aakashyap <akash.1234@live.com>
2020-05-09 19:02:42 +0530

    Day 9

---
+++ straight_line.cpp
@@ -0,0 +1,53 @@
+/*
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+Example 1:
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+
+Constraints:
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+
+*/
+
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int i;
+        double slope;
+        double point_slope;
+        int size = coordinates.size();
+        if (size == 2) return true;
+        int y_slope = (coordinates[1][1]-coordinates[0][1]);
+        int x_slope = (coordinates[1][0]-coordinates[0][0]);
+        if (x_slope != 0) {
+            slope =static_cast<double>(y_slope)/x_slope;
+        }
+        for (i=2;i<size;++i) {
+            if (x_slope == 0 || y_slope ==  0) {
+                if (x_slope == 0) {
+                    if (coordinates[i][0] != coordinates[0][0]) return false;
+                } else {
+                    if (coordinates[i][1] != coordinates[0][1]) return false;
+                }
+            } else {
+                int y = (coordinates[i][1]-coordinates[i-1][1]);
+                int x = (coordinates[i][0]-coordinates[i-1][0]);
+                if (x == 0) return false;
+                else {
+                    point_slope = static_cast<double>(y)/x;
+                }
+                if (point_slope != slope) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
